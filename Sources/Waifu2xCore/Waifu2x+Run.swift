1fa9fb87fd2204e1a0c1c0eb074f7ad858342990
vuhe <zhuhe202@qq.com>
2025-02-08 11:56:31 +0800

    fix: swift6 check error

--- Sources/Waifu2xCore/Waifu2x+Run.swift
+++ Sources/Waifu2xCore/Waifu2x+Run.swift
@@ -15,33 +15,37 @@ public extension Waifu2x {
         else { throw Waifu2xError.getCGImageFailed }
         return try await run(cgimg)
     }
-
-    /// Processing method applicable to GCD.
-    ///
-    /// This method will block the thread. Do not call it in the Task context.
-    func runAndWait(_ data: Data) throws -> Waifu2xData {
-        guard let imageSource = CGImageSourceCreateWithData(data as CFData, nil),
-              let cgimg = CGImageSourceCreateImageAtIndex(imageSource, 0, nil)
-        else { throw Waifu2xError.getCGImageFailed }
-        return try runAndWait(cgimg)
-    }
-
-    /// Processing method applicable to GCD.
-    ///
-    /// This method will block the thread. Do not call it in the Task context.
-    func runAndWait(_ image: CGImage) throws -> Waifu2xData {
-        var result: Result<Waifu2xData, any Error> = .failure(Waifu2xError.blockRunFailed)
-        let group = DispatchGroup()
-        group.enter()
-        Task {
-            do {
-                result = try await .success(run(image))
-            } catch {
-                result = .failure(error)
-            }
-            group.leave()
-        }
-        group.wait()
-        return try result.get()
-    }
 }
+
+#if swift(<6.0)
+    public extension Waifu2x {
+        /// Processing method applicable to GCD.
+        ///
+        /// This method will block the thread. Do not call it in the Task context.
+        func runAndWait(_ data: Data) throws -> Waifu2xData {
+            guard let imageSource = CGImageSourceCreateWithData(data as CFData, nil),
+                  let cgimg = CGImageSourceCreateImageAtIndex(imageSource, 0, nil)
+            else { throw Waifu2xError.getCGImageFailed }
+            return try runAndWait(cgimg)
+        }
+
+        /// Processing method applicable to GCD.
+        ///
+        /// This method will block the thread. Do not call it in the Task context.
+        func runAndWait(_ image: CGImage) throws -> Waifu2xData {
+            var result: Result<Waifu2xData, any Error> = .failure(Waifu2xError.blockRunFailed)
+            let group = DispatchGroup()
+            group.enter()
+            Task {
+                do {
+                    result = try await .success(run(image))
+                } catch {
+                    result = .failure(error)
+                }
+                group.leave()
+            }
+            group.wait()
+            return try result.get()
+        }
+    }
+#endif
